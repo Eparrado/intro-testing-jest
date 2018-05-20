1a6d4ef64fe0fedb8331c8fbd4d1f99ab1432778
Estela Parrado <estela.parrado@gmail.com>
2018-05-20 13:55:57 +0200

    es6kata: block scope

--- index.test.js
+++ index.test.js
@@ -554,3 +554,91 @@ describe('arrow functions have lexical `this`, no dynamic `this`', () => {
   });
 
 });
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Block scope let
+describe('`let` restricts the scope of the variable to the current block', () => {
+
+  describe('`let` vs. `var`', () => {
+
+    it('`var` works as usual', () => {
+      if (true) {
+        var varX = true;
+      }
+      assert.equal(varX, true);
+    });
+
+    it('`let` restricts scope to inside the block', () => {
+      if (true) {
+        let letX = true;
+      }
+      assert.throws(() => console.log(letX));
+    });
+
+  });
+
+  describe('`let` usage', () => {
+
+    it('`let` use in `for` loops', () => {
+      let obj = { x: 1 };
+      for (let key in obj) { }
+      assert.throws(() => console.log(key));
+    });
+
+    it('create artifical scope, using curly braces', () => {
+      {
+        let letX = true;
+      }
+      assert.throws(() => console.log(letX));
+    });
+
+  });
+
+});
+
+//Block scope const
+describe('`const` is like `let` plus read-only', () => {
+
+  describe('scalar values are read-only', () => {
+
+    it('number', () => {
+      const constNum = 0;
+      //constNum = 1;
+      assert.equal(constNum, 0);
+    });
+
+    it('string', () => {
+      const constString = 'I am a const';
+      //constString = 'Cant change you?';
+      assert.equal(constString, 'I am a const');
+    });
+
+  });
+
+  const notChangeable = 23;
+
+  it('const scope leaks too', () => {
+    assert.equal(notChangeable, 23);
+  });
+
+  describe('complex types are NOT fully read-only', () => {
+
+    it('array', () => {
+      const arr = [4, 23];
+      arr[0] = 42;
+      assert.equal(arr[0], 42);
+    });
+
+    it('object', () => {
+      const obj = { x: 1 };
+      obj.x = 3;
+      assert.equal(obj.x, 3);
+    });
+
+  });
+
+});
