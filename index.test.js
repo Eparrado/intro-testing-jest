2ddc89ca30a04947035207b521adc9570d2e7216
Estela Parrado <estela.parrado@gmail.com>
2018-05-20 13:48:42 +0200

    es6kata: arrow functions

--- index.test.js
+++ index.test.js
@@ -473,3 +473,84 @@ describe('`str.endsWith(searchString)` determines whether `str` ends with `searc
   });
 
 });
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Arrow functions basis
+describe('arrow functions', function () {
+
+  it('are shorter to write', function () {
+    var func = () => {
+      return 'I am func';
+    };
+    assert.equal(func(), 'I am func');
+  });
+
+  it('a single expression, without curly braces returns too', function () {
+    var func = () => 'I return too';
+    assert.equal(func(), 'I return too');
+  });
+
+  it('one parameter can be written without parens', () => {
+    var func = p => p - 1;
+    assert.equal(func(25), 24);
+  });
+
+  it('many params require parens', () => {
+    var func = (param, param1) => param + param1;
+    assert.equal(func(23, 42), 23 + 42);
+  });
+
+  it('body needs parens to return an object', () => {
+    var func = () => {
+      return {
+        iAm: 'an object'
+      }
+    };
+    assert.deepEqual(func(), { iAm: 'an object' });
+  });
+
+});
+
+//Arrow functions binding
+class LexicallyBound {
+
+  getFunction() {
+    return () => this
+  }
+
+  getArgumentsFunction() {
+    return () => arguments
+  }
+
+}
+
+describe('arrow functions have lexical `this`, no dynamic `this`', () => {
+
+  it('bound at definition time, use `=>` ', function () {
+    var bound = new LexicallyBound();
+    var fn = bound.getFunction();
+
+    assert.strictEqual(fn(), bound);
+  });
+
+  it('can NOT bind a different context', function () {
+    var bound = new LexicallyBound();
+    var fn = bound.getFunction();
+    var anotherObj = {};
+    var expected = bound;
+
+    assert.strictEqual(fn.call(anotherObj), expected);
+  });
+
+  it('`arguments` doesnt work inside arrow functions', function () {
+    var bound = new LexicallyBound();
+    var fn = bound.getArgumentsFunction();
+
+    assert.equal(fn(1, 2).length, 0);
+  });
+
+});
